a0c0d580dbc0aeacbc5c74ab7f2699d644c05f6b
Gaffix <88161942+Gaffix@users.noreply.github.com>
2025-07-15 14:53:50 -0300

    Colocado css no novo arquivo e ajustes

--- placeholder.jsx
+++ placeholder.jsx
@@ -1,9 +1,21 @@
+import React from 'react';
+
 import { createRoot } from 'react-dom/client';
-import Button from './components/button';
+import Botao from './components/button';
 // nao tem ainda ^
 
-const ButtonDiv = document.getElementById('botao-continuar');
-if (ButtonDiv) {
-  createRoot(ButtonDiv).render(<Button/>);
+const BotaoContinuarDiv = document.getElementById('botao-continuar');
+if (BotaoContinuarDiv) {
+  createRoot(BotaoContinuarDiv).render(<Botao texto="Continuar" backgroundColor="#000000" color="#ffffff"/>);
+}
+
+const BotaoLoginDiv = document.getElementById('botao-fazerlogin');
+if (BotaoLoginDiv) {
+  createRoot(BotaoLoginDiv).render(<Botao texto="Fazer Login" backgroundColor="#ffffff" color="#000000"/>);
+}
+
+const BotaoCriarContaDiv = document.getElementById('botao-criarconta');
+if (BotaoCriarContaDiv) {
+  createRoot(BotaoCriarContaDiv).render(<Botao texto="Criar Conta" backgroundColor="#ffffff" color="#000000"/>);
 }
 // coloca o componente do botao na div ^
