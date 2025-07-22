9294abe5666e186d4fe70bef5d3b013c7164c76c
Qlqr1 <guto.kg2008@gmail.com>
2025-07-21 22:18:08 -0300

    Tranformei o cadastroVendedor em .html

---
+++ pages/cadastroVendedor.js
@@ -0,0 +1,53 @@
+document.getElementById("outrosCheckbox").addEventListener("change", function () {
+    const outrosInput = document.getElementById("outrosInput");
+    if (this.checked) {
+        outrosInput.style.display = "block";
+    } else {
+        outrosInput.style.display = "none";
+        outrosInput.value = "";
+  }
+});
+
+
+document.getElementById("meuFormulario").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+
+    const textInputs = this.querySelectorAll('input[type="text"]');
+    const checkboxes = this.querySelectorAll('input[type="checkbox"]');
+    const outrosCheckbox = document.getElementById("outrosCheckbox");
+    const outrosInput = document.getElementById("outrosInput");
+
+
+    for (let input of textInputs) {
+        if (input !== outrosInput && input.value.trim() === "") {
+        alert("É obrigatório preencher todos os campos.");
+        input.focus();
+        return;
+        }
+    }
+
+
+    let algumMarcado = false;
+    for (let checkbox of checkboxes) {
+        if (checkbox.checked) {
+        algumMarcado = true;
+        break;
+        }
+    }
+    if (!algumMarcado) {
+        alert("Selecione pelo menos uma opção.");
+        return;
+    }
+
+
+    if (outrosCheckbox.checked && outrosInput.value.trim() === "") {
+        alert("Você marcou 'Outros'. Especifique o conteúdo.");
+        outrosInput.focus();
+        return;
+    }
+
+
+    alert("Formulário válido! Pode ser enviado.");
+});
+
